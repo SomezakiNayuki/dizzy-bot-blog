add36a7b7be8165363c9540caaddfcdc1aa0ca74
Henry <sunhaoyu468@gmail.com>
2023-11-24 21:42:36 +1100

    generalized create content component to read configuration from json file

---
+++ dizzy-bot-blog-frontend/src/app/services/form-definition.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FormDefinition } from '../models/form-definition';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormDefinitionService {
+
+  constructor(private http: HttpClient) { }
+
+  public getBlogFormDefinitions(): Observable<FormDefinition[]> {
+    return this.http.get<FormDefinition[]>('assets/form-definitions/blogFormDefinitions.json');
+  }
+
+}
